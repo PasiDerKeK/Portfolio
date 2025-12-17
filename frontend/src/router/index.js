d3c145d005bba0024eef4c9eb94bdc0311559545
PasiDerDev <pascal.k@gmx.de>
2025-12-17 19:51:15 +0100

    Finally ready

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,13 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
+
 import HomePage from "../pages/HomePage.vue";
 import ProjectsPage from "../pages/ProjectsPage.vue";
 import LanguagesPage from "../pages/LanguagesPage.vue";
 import ContactPage from "../pages/ContactPage.vue";
 
-const base = import.meta.env.BASE_URL; // nimmt /Portfolio/ in production, / in dev
-
-export default createRouter({
-    history: createWebHistory(base),
+const router = createRouter({
+    history: createWebHistory("/"),
     routes: [
         { path: "/", component: HomePage },
         { path: "/projects", component: ProjectsPage },
@@ -15,3 +14,5 @@ export default createRouter({
         { path: "/contact", component: ContactPage }
     ]
 });
+
+export default router;
