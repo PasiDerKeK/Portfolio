98bbf4a75237cea0c1bc3d0e105dcad5da095850
PasiDerDev <pascal.k@gmx.de>
2025-12-17 18:27:33 +0100

    Finally ready

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,9 +1,17 @@
-import { createRouter, createWebHashHistory } from "vue-router";
-// statt createWebHistory
+import { createRouter, createWebHistory } from "vue-router";
+import HomePage from "../pages/HomePage.vue";
+import ProjectsPage from "../pages/ProjectsPage.vue";
+import LanguagesPage from "../pages/LanguagesPage.vue";
+import ContactPage from "../pages/ContactPage.vue";
 
-const router = createRouter({
-    history: createWebHashHistory(),
-    routes
+const base = import.meta.env.BASE_URL; // nimmt /Portfolio/ in production, / in dev
+
+export default createRouter({
+    history: createWebHistory(base),
+    routes: [
+        { path: "/", component: HomePage },
+        { path: "/projects", component: ProjectsPage },
+        { path: "/languages", component: LanguagesPage },
+        { path: "/contact", component: ContactPage }
+    ]
 });
-
-export default router;
